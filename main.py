fb852e458f04a99f53269113790ca1d0a1b2704b
Jan Podávka <72079041+JanPodavka@users.noreply.github.com>
2024-05-06 15:43:57 +0200

    mongoDB init

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import json
+import pyjson5 as json5
+import pymongo
+from pymongo.collection import Collection
+import pandas as pd
+
+
+def saveJsonToMongo(path,collection):
+    with open(path, 'r') as file:
+        file_data = json.load(file)
+    inserted = collection.insert_many(file_data)
+    print(inserted.inserted_ids)
+
+
+
+
 if __name__ == '__main__':
-    print("Hello world")
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")  # zakomentovat po vytvřoení
+    mydb = myclient["RDBsemestral"]
+    mycol = mydb["TollGates"]
+    saveJsonToMongo('data/data-export2.json',mycol)
