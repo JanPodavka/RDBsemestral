07fb88b3b1ab1c4aa57503425edc0f446653bc88
OndraSoukup <ondrej.soukup@tul.cz>
2024-05-08 01:09:03 +0200

    SPZ_Data

--- main.py
+++ main.py
@@ -10,28 +10,6 @@ def saveJsonToMongo(path, collection):
     print(inserted.inserted_ids)
 
 
-def SPZ():
-    conn = psycopg2.connect(database="postgres",
-                            host="localhost",
-                            user="postgres",
-                            password="root",
-                            port="5432")
-    cursor = conn.cursor()
-    select_stmt = (
-        "SELECT spz FROM Vozidlo"
-    )
-    cursor.execute(select_stmt)
-    spz_results = cursor.fetchall()
-    spz_dict = {"spz": []}
-
-    for row in spz_results:
-        spz_dict["spz"].append(row[0])
-
-    cursor.close()
-    conn.close()
-    print(spz_dict)
-
-
 def MongoToPostgre(collection):
     conn = psycopg2.connect(database="postgres",
                             host="localhost",
@@ -76,10 +54,84 @@ def MongoToPostgre(collection):
     conn.close()
 
 
+def SPZ():
+    conn = psycopg2.connect(database="postgres",
+                            host="localhost",
+                            user="postgres",
+                            password="root",
+                            port="5432")
+    cursor = conn.cursor()
+    select_stmt = (
+        "SELECT spz FROM Vozidlo"
+    )
+    cursor.execute(select_stmt)
+    spz_results = cursor.fetchall()
+    spz_dict = {"spz": []}
+
+    for row in spz_results:
+        spz_dict["spz"].append(row[0])
+
+    cursor.close()
+    conn.close()
+    # print(spz_dict)
+    return spz_dict
+
+def SPZ_Data(spz):
+    conn = psycopg2.connect(database="postgres",
+                            host="localhost",
+                            user="postgres",
+                            password="root",
+                            port="5432")
+    cursor = conn.cursor()
+    spz_prujezd = {}
+
+    sql_query = (
+    """
+    SELECT SUM(ujete_km) AS celkem_km
+    FROM Prujezd
+    WHERE Vozidlo_SPZ = %s
+    """
+    )
+    cursor.execute(sql_query, (spz,))
+    celkem_km = cursor.fetchone()[0]
+    spz_prujezd["celkem_km"] = celkem_km
+
+    sql_query = (
+        """
+        SELECT Kredit FROM Vozidlo
+        WHERE SPZ = %s
+        """
+    )
+    cursor.execute(sql_query, (spz,))
+    kredity = cursor.fetchone()[0]
+    spz_prujezd["kredity"] = kredity
+
+    sql_query = (
+        """
+        SELECT datum_prujezdu, brana_id, ujete_km FROM Prujezd
+        WHERE Vozidlo_SPZ = %s
+        """
+    )
+    cursor.execute(sql_query, (spz,))
+    data = cursor.fetchall()
+    data_dict = []
+
+    for row in data:
+        datum_prujezdu, brana_id, ujete_km = row
+        prujezd_dict = {"datum_prujezdu": datum_prujezdu, 'brana_id': brana_id, 'ujete_km': ujete_km}
+        data_dict.append(prujezd_dict)
+
+    spz_prujezd["prujezd"] = data_dict
+    cursor.close()
+    conn.close()
+    return spz_prujezd
+
+
 if __name__ == '__main__':
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")  # zakomentovat po vytvřoení
     mydb = myclient["RDBsemestral"]
     mycol = mydb["TollGates"]
     # saveJsonToMongo('data/data-export2.json', mycol)
     # MongoToPostgre(mycol)
-    SPZ()
+    print(SPZ())
+    print(SPZ_Data('QQQ4567'))
